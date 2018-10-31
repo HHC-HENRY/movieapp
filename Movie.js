ebb638ea5fc1de5ea36005eb01e5e464e66566e3
HHC-HENRY <henrpotter01@yahoo.com.tw>
2018-10-31 12:57:19 +0800

    Initial commit

---
+++ Movie.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const db = 'mongodb://Henry:philips01@ds241133.mlab.com:41133/henrymovie'
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(error => {
+    console.log('Mongoose connetion error: ', error);
+  });
+
+const schema = mongoose.Schema({
+  title: { type: String },
+  year: { type: String },
+  genre: { type: String },
+  actors: { type: String },
+  plot: { type: String },
+  poster: { type: String }
+});
+
+const Movie = mongoose.model('Movie', schema, 'movieCollection');
+
+module.exports = Movie;
